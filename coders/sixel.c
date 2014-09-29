5dda588499d072d05b40c7ab9d6cc4401b742d7b
Hayaki Saito <user@zuse.jp>
2014-09-30 00:51:29 +0900

    Register "SIX" as extension of SIXEL

--- coders/sixel.c
+++ coders/sixel.c
@@ -1147,6 +1147,14 @@ ModuleExport size_t RegisterSIXELImage(void)
   entry->description=ConstantString("DEC SIXEL Graphics Format");
   entry->module=ConstantString("SIXEL");
   (void) RegisterMagickInfo(entry);
+  entry=SetMagickInfo("SIX");
+  entry->decoder=(DecodeImageHandler *) ReadSIXELImage;
+  entry->encoder=(EncodeImageHandler *) WriteSIXELImage;
+  entry->magick=(IsImageFormatHandler *) IsSIXEL;
+  entry->adjoin=MagickFalse;
+  entry->description=ConstantString("DEC SIXEL Graphics Format");
+  entry->module=ConstantString("SIX");
+  (void) RegisterMagickInfo(entry);
   return(MagickImageCoderSignature);
 }
 
